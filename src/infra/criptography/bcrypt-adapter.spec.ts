0422f00f1f0ef470d6e3f2dcdbd8a81556e36e6d
Danilo Oliveira de Mello Amorim <danilo.om.amorim.br@gmail.com>
2021-07-07 09:52:57 -0300

    refactor: change return throwError

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+export const throwError = (): never => {
+  throw new Error()
+}
+
 const salt = 12
 
 jest.mock('bcrypt', () => ({
@@ -25,5 +29,12 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_values')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
