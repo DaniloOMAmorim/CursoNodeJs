5edfc5353ff4a2f4dc1cb179440ab0cd21a0a209
Danilo Oliveira de Mello Amorim <danilo.om.amorim.br@gmail.com>
2021-07-07 10:48:19 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  cliente: null as unknown as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.cliente = await MongoClient.connect(process.env.MONGO_URL ? process.env.MONGO_URL : '', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.cliente.close()
+  }
+}
