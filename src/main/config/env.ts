c12247335e88bcc45fd121ce83c4b8d301c3ca41
Danilo Oliveira de Mello Amorim <danilo.om.amorim.br@gmail.com>
2021-07-19 17:37:56 -0300

    refactor: test env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'errVar',
   saltBcrypt: parseInt(process.env.BCRYPT_SALT) || 12
